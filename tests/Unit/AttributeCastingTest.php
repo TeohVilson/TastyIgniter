31559e5b09e6c1544681712b0d9e3d05821b0acf
TeohVilson <vilsonteoh3@gmail.com>
2025-04-07 04:26:29 +0800

    Create AttributeCastingTest.php

---
+++ tests/Unit/AttributeCastingTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Date;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_casts_email_verified_at_to_datetime()
+    {
+        $user = User::create([
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        // Assert that email_verified_at is an instance of Carbon (datetime)
+        $this->assertInstanceOf(\Carbon\Carbon::class, $user->email_verified_at);
+    }
+}
